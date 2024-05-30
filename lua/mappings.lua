7bb01fdfde29f1714349e6cb4962fdee5f812421
Alexander Goscinski <alex.goscinski@posteo.de>
2024-05-30 08:51:32 +0200

    overwrite ctrl + c command to do nothing in normal mode and use ctrl +x instead for yanking entire file

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,9 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- custom
+
+-- moves yanking behavior in normal mode to ctrl + x
+map("n", "<C-c>", "", { desc = "Nothing" })
+map("n", "<C-x>", 'gg"+yG', { desc = "Yank entire file" })
