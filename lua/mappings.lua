430e9d5890937432a572ff0c893453bb04279145
Alexander Goscinski <alex.goscinski@posteo.de>
2024-05-30 09:12:15 +0200

    remove yanking behavior from delete

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -14,3 +14,16 @@ map("i", "jk", "<ESC>")
 -- moves yanking behavior in normal mode to ctrl + x
 map("n", "<C-c>", "", { desc = "Nothing" })
 map("n", "<C-x>", 'gg"+yG', { desc = "Yank entire file" })
+
+-- Prevents the commands that delete content to also yank it.
+-- The old behavior yanking the deleted content is mapped to <leader> + <command>.
+-- Only the x command is not remapped since it conflicts with closing a buffer.
+--
+map("n", "x", "\"_x", { desc = "Delete character"})
+map("n", "d", "\"_d", { desc = "Delete text"})
+map("v", "d", "\"_d", { desc = "Delete text"})
+map("n", "D", "\"_D", { desc = "Delete line"})
+
+map("n", "<leader>d", "\"\"d", { desc = "Delete and yank text"})
+map("v", "<leader>d", "\"\"d", { desc = "Delete and yank text"})
+map("n", "<leader>D", "\"\"D", { desc = "Delete and yank line"})
